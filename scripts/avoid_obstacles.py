57f6346b3ba4b45e9d9575687150287210ce98a8
bsh75 <bsh75@Ubuntu.myguest.virtualbox.org>
2024-06-04 17:55:30 +0900

    Basic

--- scripts/avoid_obstacles.py
+++ scripts/avoid_obstacles.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -16,14 +16,27 @@ class ObstacleAvoider:
         self.goal = MoveBaseGoal()
 
     def scan_callback(self, data):
-        twist = Twist()
-        if min(data.ranges) < 0.5:
-            twist.angular.z = 0.5  # Turn
-            twist.linear.x = 0.0
+        vel_cmd = Twist()
+        # rospy.loginfo(f"\n\nDATA is: {data}\n\n")
+        front_angle_low = 10 * (3.14159 / 180)  # +ve direction from origin
+        front_angle_high = 3.14159 - 10 * (3.14159 / 180)  # negative direction from origin
+        angle_increment = 0.017501922324299812 # Radians
+        ranges_infront = []
+        for i, range in enumerate(data.ranges):
+            angle = i * angle_increment
+            if angle > front_angle_high or angle < front_angle_low:
+                ranges_infront.append(angle)
+        rospy.loginfo(f"Ranges infront: {ranges_infront} has length {len(ranges_infront)} out of total {len(data.ranges)}")
+        range_front = sum(ranges_infront) / len(ranges_infront) 
+
+        rospy.loginfo(f"Low average : {range_front} lowest: {data.range_min}\n which is interesting\n\n")
+        if (range_front) < 0.4:
+            vel_cmd.angular.z = 0.5  # Turn
+            vel_cmd.linear.x = 0.0
         else:
-            twist.linear.x = 0.2  # Move forward
-            twist.angular.z = 0.0
-        self.cmd_pub.publish(twist)
+            vel_cmd.linear.x = 0.2  # Move forward
+            vel_cmd.angular.z = 0.0
+        self.cmd_pub.publish(vel_cmd)
 
     def move_to_goal(self, x, y, w):
         self.goal.target_pose.header.frame_id = 'map'
