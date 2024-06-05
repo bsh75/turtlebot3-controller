421058cf8b95812fce88d8e5766b83ed48ff5e9c
bsh75 <bsh75@Ubuntu.myguest.virtualbox.org>
2024-06-05 11:09:43 +0900

    Unidirectional Rumba type functionality achieved, with stopping on exit

--- scripts/avoid_obstacles.py
+++ scripts/avoid_obstacles.py
@@ -5,38 +5,46 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
+import time
 
 class ObstacleAvoider:
     def __init__(self):
+        rospy.on_shutdown(self.clean_shutdown)
         rospy.init_node('obstacle_avoider')
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.vel_msg = Twist()
+
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.client.wait_for_server()
         self.goal = MoveBaseGoal()
+        rospy.spin()
 
     def scan_callback(self, data):
-        vel_cmd = Twist()
         # rospy.loginfo(f"\n\nDATA is: {data}\n\n")
-        front_angle_low = 10 * (3.14159 / 180)  # +ve direction from origin
-        front_angle_high = 3.14159 - 10 * (3.14159 / 180)  # negative direction from origin
-        angle_increment = 0.017501922324299812 # Radians
-        ranges_infront = []
-        for i, range in enumerate(data.ranges):
-            angle = i * angle_increment
-            if angle > front_angle_high or angle < front_angle_low:
-                ranges_infront.append(angle)
-        rospy.loginfo(f"Ranges infront: {ranges_infront} has length {len(ranges_infront)} out of total {len(data.ranges)}")
-        range_front = sum(ranges_infront) / len(ranges_infront) 
-
-        rospy.loginfo(f"Low average : {range_front} lowest: {data.range_min}\n which is interesting\n\n")
+        range_front = data.ranges[0]
+        rospy.loginfo(f"Range infront: {range_front} lowest: {data.range_min}\n which is interesting\n\n")
         if (range_front) < 0.4:
-            vel_cmd.angular.z = 0.5  # Turn
-            vel_cmd.linear.x = 0.0
+            self.vel_msg.angular.z = 0.5  # Turn
+            self.vel_msg.linear.x = 0.0
+            self.publish_vel("turning")
         else:
-            vel_cmd.linear.x = 0.2  # Move forward
-            vel_cmd.angular.z = 0.0
-        self.cmd_pub.publish(vel_cmd)
+            self.vel_msg.linear.x = 0.2  # Move forward
+            self.vel_msg.angular.z = 0.0
+            self.publish_vel("fowards")
+
+
+    def publish_vel(self, msg_type="move"):
+        ''' Publish the Twist message '''
+        rospy.loginfo("Publishing %s message..." % msg_type)
+        self.cmd_pub.publish(self.vel_msg)  # Directly publish without the loop
+
+
+    def clean_shutdown(self):
+        self.vel_msg = Twist()
+        rospy.loginfo(f"Stopping {self.vel_msg}")
+        self.publish_vel("stop")
 
     def move_to_goal(self, x, y, w):
         self.goal.target_pose.header.frame_id = 'map'
@@ -50,7 +58,7 @@ class ObstacleAvoider:
 
 if __name__ == '__main__':
     try:
-        avoider = ObstacleAvoider()
-        rospy.spin()
+        ObstacleAvoider()
+
     except rospy.ROSInterruptException:
         pass
