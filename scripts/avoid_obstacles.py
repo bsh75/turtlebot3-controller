98970dec8bab8342ee97dbb0ddf8c227f008ed1b
bsh75 <bsh75@Ubuntu.myguest.virtualbox.org>
2024-06-05 18:01:16 +0900

    Made RobotParameters class for physical parameters relating to the robot. Should be determined from the launch file in future

--- scripts/avoid_obstacles.py
+++ scripts/avoid_obstacles.py
@@ -7,10 +7,18 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 import math
 
+class RobotParameters:
+    '''Should really be determined from launch in relation to the robot type'''
+    def __init__(self):
+        self.linear_v_lim = 0.3
+        self.angular_v_lim = 0.5
+        self.range_lim = 0.5
+
 class ObstacleAvoider:
     def __init__(self):
         '''Initialises node. Creats publisher, velocity message, subscriber, client and goal objects.
             Also sets shutdown callback function'''
+        self.phys_params = RobotParameters()
         rospy.on_shutdown(self.clean_shutdown)
         rospy.init_node('obstacle_avoider')
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
@@ -26,11 +34,6 @@ class ObstacleAvoider:
     def linear_ctl_rumba_move(self, ranges):
         '''Simple Rumba ctl method for controlling robots movements from a list of ranges. 
         Assumes: 0th index is directly infront with 360 measurements evenly spread around robot, +ve index direction is same as +ve rotation direction about z axis'''
-        # Paramaters for velocity calculation (dependant on robots characteristics) -  should be defined in robot class
-        linear_cap = 0.3
-        angular_cap = 0.5
-        range_lim = 0.5
-
         # Find average across 10 measurements spanning 10deg in desired directions
         ranges_front = ranges[:5] + ranges[-5:]
         range_front = sum(ranges_front)/len(ranges_front)
@@ -40,30 +43,32 @@ class ObstacleAvoider:
         range_front_left = sum(ranges_front_left)/len(ranges_front_left)
 
         # Move straight when no obstacles in front facing limits
-        if (range_front > range_lim) and (range_front_right > range_lim) and (range_front_left > range_lim):
-            self.vel_msg.linear.x = linear_cap
+        if (range_front > self.phys_params.range_lim) and (range_front_right > self.phys_params.range_lim) and (range_front_left > self.phys_params.range_lim):
+            self.vel_msg.linear.x = self.phys_params.linear_v_lim
             self.vel_msg.angular.z = 0.0
-            rospy.loginfo("Straight")
+            msg_type = "Straight"
         # Turn left: Smaller range_front_right means tighter turn (reversing included)
-        elif (range_front > range_lim) and (range_front_right < range_lim) and (range_front_left > range_lim):
-            self.vel_msg.linear.x = 2*range_front_right*linear_cap/range_lim - linear_cap
-            self.vel_msg.angular.z = angular_cap * (1-range_front_right/range_lim)
-            rospy.loginfo("Ctl left")
+        elif (range_front > self.phys_params.range_lim) and (range_front_right < self.phys_params.range_lim) and (range_front_left > self.phys_params.range_lim):
+            self.vel_msg.linear.x = 2*range_front_right*self.phys_params.linear_v_lim/self.phys_params.range_lim - self.phys_params.linear_v_lim
+            self.vel_msg.angular.z = self.phys_params.angular_v_lim * (1-range_front_right/self.phys_params.range_lim)
+            msg_type = "Ctl left"
         # Turn right: Smaller range_front_left means tighter turn (reversing included)
-        elif (range_front > range_lim) and (range_front_right > range_lim) and (range_front_left < range_lim):
-            self.vel_msg.linear.x = 2*range_front_left*linear_cap/range_lim - linear_cap
-            self.vel_msg.angular.z = -(angular_cap * (1-range_front_left/range_lim))
-            rospy.loginfo("Ctl right")
+        elif (range_front > self.phys_params.range_lim) and (range_front_right > self.phys_params.range_lim) and (range_front_left < self.phys_params.range_lim):
+            self.vel_msg.linear.x = 2*range_front_left*self.phys_params.linear_v_lim/self.phys_params.range_lim - self.phys_params.linear_v_lim
+            self.vel_msg.angular.z = -(self.phys_params.angular_v_lim * (1-range_front_left/self.phys_params.range_lim))
+            msg_type = "Ctl right"
         # Hard turn left when obstacles infront (could change this for better coverage)
         else:
             self.vel_msg.linear.x = -0.1
-            self.vel_msg.angular.z = angular_cap
-            rospy.loginfo("Hard left")
-        
-        # Log ranges
-        rospy.loginfo(f"\nFront: {range_front}\nLleft: {range_front_left}, Right: {range_front_right}")
-        # Publish velocity and display commanded velocities
-        self.publish_vel(f"linear: {self.vel_msg.linear.x}, angular: {self.vel_msg.angular.z}")
+            self.vel_msg.angular.z = self.phys_params.angular_v_lim
+            msg_type = "Hard left"
+        try:
+            # Log ranges
+            rospy.loginfo(f"\nFront: {range_front}\nLleft: {range_front_left}, Right: {range_front_right}")
+            # Publish velocity and display commanded velocities
+            self.publish_vel(msg_type)
+        except rospy.ROSException as e:
+            rospy.logerr("Failed to publish velocity command: %s", e)
 
 
     def scan_callback(self, data):
@@ -76,7 +81,7 @@ class ObstacleAvoider:
 
     def publish_vel(self, msg_type="move"):
         ''' Publish the Twist message '''
-        rospy.loginfo("Publishing velocity: %s\n" % msg_type)
+        rospy.loginfo(f"{msg_type} movement: linear: {self.vel_msg.linear.x}, angular: {self.vel_msg.angular.z}")
         self.cmd_pub.publish(self.vel_msg)  # Directly publish without the loop
 
 
