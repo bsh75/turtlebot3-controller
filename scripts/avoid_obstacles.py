54c040e14b3ea384367a64a482de03a692d9fa6c
bsh75 <bsh75@Ubuntu.myguest.virtualbox.org>
2024-06-05 12:01:48 +0900

    Basic 2D Rumba functionality. Needs adjustment to favour forwards direction, and addition of front-side sensors in velocity calcs

--- scripts/avoid_obstacles.py
+++ scripts/avoid_obstacles.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
-import time
+import math
 
 class ObstacleAvoider:
     def __init__(self):
@@ -23,21 +23,36 @@ class ObstacleAvoider:
 
     def scan_callback(self, data):
         # rospy.loginfo(f"\n\nDATA is: {data}\n\n")
+        data.ranges = [r if not math.isinf(r) else data.range_max for r in data.ranges]
         range_front = data.ranges[0]
-        rospy.loginfo(f"Range infront: {range_front} lowest: {data.range_min}\n which is interesting\n\n")
-        if (range_front) < 0.4:
-            self.vel_msg.angular.z = 0.5  # Turn
-            self.vel_msg.linear.x = 0.0
-            self.publish_vel("turning")
-        else:
-            self.vel_msg.linear.x = 0.2  # Move forward
-            self.vel_msg.angular.z = 0.0
-            self.publish_vel("fowards")
+        range_back = data.ranges[180]
+        range_front_right = data.ranges[-45]
+        range_front_left = data.ranges[45]
+        range_right = data.ranges[-90]
+        range_left = data.ranges[90]
+        rospy.loginfo(f"\nInfront: {range_front}, Behind: {range_back}\nFrontleft: {range_front_left}, Frontright: {range_front_right}\nRight: {range_right}, Left: {range_left}")
+        linear_scalar = 0.01
+        angular_scalar = 0.1
+        
+        self.vel_msg.linear.x += linear_scalar*(range_front-range_back)
+        if self.vel_msg.linear.x > 0.3: 
+            self.vel_msg.linear.x = 0.3
+        elif self.vel_msg.linear.x < -0.3:
+            self.vel_msg.linear.x = -0.3
+        # self.vel_msg.linear += 0.5*linear_scalar*(range_front_right-range_back)
+
+        self.vel_msg.angular.z += angular_scalar*(range_left - range_right)
+        if self.vel_msg.angular.z > 0.3: 
+            self.vel_msg.angular.z = 0.3
+        elif self.vel_msg.angular.z < -0.3:
+            self.vel_msg.angular.z = -0.3
+
+        self.publish_vel(f"linear: {self.vel_msg.linear}, angular: {self.vel_msg.angular}")
 
 
     def publish_vel(self, msg_type="move"):
         ''' Publish the Twist message '''
-        rospy.loginfo("Publishing %s message..." % msg_type)
+        rospy.loginfo("Publishing velocity: %s\n" % msg_type)
         self.cmd_pub.publish(self.vel_msg)  # Directly publish without the loop
 
 
